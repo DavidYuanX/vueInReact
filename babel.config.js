a8c90ecf1f13630735fb0b19085a156410b33526
Daivd Yuan <Summer_xx@163.com>
2021-05-08 00:45:16 +0800

    vue in react

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
+  // presets: [
+  //   '@vue/cli-plugin-babel/preset'
+  // ]
+  // presets: [
+  //   ['@babel/preset-react'],
+  //   ['@babel/preset-env']
+    // '@vue/app'
+  // ],
+  plugins: [
+    ['transform-react-jsx'],
+    ['@babel/plugin-syntax-dynamic-import']
   ]
 }
--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,23 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <my-react-component :message="message" />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import { ReactInVue } from 'vuera'
+import reactTest from './react_test'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data () {
+    return {
+      message: 'Hello from React!'
+    }
+  },
+  components: { 'my-react-component': ReactInVue(reactTest), HelloWorld }
 }
 </script>
 
